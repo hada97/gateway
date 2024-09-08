54e1f8ce3c6df24350a6057ebc7b4a97ab74f487
Adailson <adailson65@gmail.com>
2024-09-08 00:53:13 -0300

    first commit

---
+++ src/main/java/br/com/alurafood/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package br.com.alurafood.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+		System.out.println("--------------------------");
+		System.out.println("----Executando GATEWAY----");
+		System.out.println("--------------------------");
+	}
+
+}
